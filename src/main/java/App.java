ee78d3ad299b814bf8cbde8187725d43b550b7dc
Student <example@example.com>
2018-05-25 15:45:28 -0700

    delete and create a new App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,42 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
-
-
-
 public class App {
-    public static  void main(String[] args, Object beverages, Object foods, Object entertainments, Object food, Object beverage, Object entertainment) {
-        Boolean programRunning = true;
+    public static void main(String[] args) {
+        boolean programRunning = true;
         while (programRunning) {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Welcome to portland Event planning app");
-            Party party = new Party(int guests int String food, String beverage, String entertainment);
-            try {
-                System.out.println("Enter your food course type: full-course or light-snacks");
-                String userInput = bufferedReader.readLine();
-                if(userInput.equals("book a party")) {
-                    System.out.println("How many people will be partying?");
-
-
-            }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-
-
-//        boolean foodType = true;
-//        while (foodType) {
-//
-//            if (food.equals("full-course") || food.equals("light snacks")){
-//                foodType = false;
-//            } else {
-//                System.out.println("Input not recognized");
-//            }
-//
-//        }
+            System.out.println("Welcome to portland party planner app");
+//            Party newParty = new Party();
 
         }
     }
-}
 }
