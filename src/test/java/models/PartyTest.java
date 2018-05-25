3ff0a4f7cdf90e8ed2237018e1befe5c13647b28
Student <example@example.com>
2018-05-25 11:28:35 -0700

    create a new test to see if PArty class has a correct instantiation of its new constructor

--- src/test/java/models/PartyTest.java
+++ src/test/java/models/PartyTest.java
@@ -1,7 +1,13 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PartyTest {
-
+    @Test
+    public void generate_ifItInstantiates_true() {
+        Party testParty = new Party();
+        assertEquals(true, testParty instanceof Party);
+    }
 }
