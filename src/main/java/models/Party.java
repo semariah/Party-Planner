e7519a9107724b6e824135a6e94e6c0ccca84029
Student <example@example.com>
2018-05-25 12:16:44 -0700

    test fail to calculate the cost of a full bar and live band and full course food for 20 people

--- src/main/java/models/Party.java
+++ src/main/java/models/Party.java
@@ -5,12 +5,24 @@ public class Party {
     private String typeOfFood;
     private String typeOfBeverage;
     private String typeOfEntertainment;
+    private int foodCost;
+    private int beverageCost;
+    private int entertainmentCost;
+    private int totalCost;
 
     public Party (int guests, String food, String beverage, String entertainment) {
         numberOfGuests = guests;
         typeOfFood = food;
         typeOfBeverage = beverage;
         typeOfEntertainment = entertainment;
+    }
+
+    public int calculateCost(){
+        if (typeOfFood.equals("full-course")) {
+           return foodCost = 0;
+        } else {
+           return foodCost = 45;
+        }
     }
 
 }
